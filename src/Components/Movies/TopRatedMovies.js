5125a91058739f0c088d6cd734aea4e207fadf1b
= <=>
2021-09-18 01:23:02 +0300

    f

---
+++ src/Components/Movies/TopRatedMovies.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const PopularMovies = (props) => {
+  
+    return (
+      <div className='row'>
+        {props.movies.map(movie => (
+          <div className='col-lg-2' key={movie.id}>
+            <div className='card m-3 mb-4 shadow-sm'>
+              <img
+                src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`}
+                className='card-img-top'
+                alt='Sample Movie'
+              />
+              <div className='card-body'>
+                <h5 className='card-title'>{movie.title}</h5>
+                <p className='card-text'>popularity: {movie.popularity}</p>
+                <div className='d-flex justify-content-between align-items-center'>
+                  <h2>
+                    <span className='badge bg-info'>{movie.vote_average}</span>
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+}
+
+export default PopularMovies;
---
+++ src/Components/Navbar/NavbarComponent.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './NavbarComponent.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom'
+
+const NavbarComponent = () => {
+  return (
+    <div>
+      <nav className='navbar navbar-expand-lg navbar-light'>
+        <div className='container-fluid'>
+          <div className='movie-icon'>
+            <FontAwesomeIcon icon='tape' />
+          </div>
+          <a className='navbar-brand' href='#'>
+            <h1 className='brand-name'>Movie Platform</h1>
+          </a>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+              <li className='nav-item1'>
+                <a className='nav-link active' aria-current='page' href='#'>
+                <Link 
+                  className="top-rated-name" to="/topRated" type="button"
+                  >My Favorite Movies</Link> 
+                </a>
+              </li>
+              <li className='nav-item2'>
+                <a className='nav-link' href='#'>
+                  <Link 
+                  className="top-rated-name" to="/favMovies" type="button"
+                  >Top Rated Movies</Link>
+                </a>
+              </li>
+            </ul>
+            <form className='d-flex'>
+              <input
+                className='form-control me-2'
+                type='search'
+                placeholder='Search a Movie'
+                aria-label='Search'
+              />
+              <button className='btn btn-outline-success' type='submit'>
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default NavbarComponent
